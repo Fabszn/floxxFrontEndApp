7f11beebdc048218c4da86635e9e96c20bb2f04c
Fabrice Sznajderman <fabszn@gmail.com>
2019-04-28 08:24:42 +0200

    test ergonomie

--- src/app/components/selections/rooms.vue
+++ src/app/components/selections/rooms.vue
@@ -1,13 +1,13 @@
 <template>
   <div>
     <Dropdown
-      :options="[{ id: 1, name: 'Option 1'}, { id: 2, name: 'Option 2'}]"
+      :options="[{ id: 1, name: '241: 9h45-12h'}, { id: 2, name: 'Maillo: 17h-18h'}, {id: 3, name: '251: 15h-18h'}]"
       v-on:selected="validateSelection"
       v-on:filter="getDropdownValues"
       :disabled="false"
-      name="zipcode"
+      name="Room"
       :maxItem="10"
-      placeholder="Please select an option"
+      placeholder="Please select a room/slot"
     ></Dropdown>
   </div>
 </template>
