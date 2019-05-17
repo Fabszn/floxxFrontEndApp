290ad54404e6b76389eab63f3cd94b677b2738ea
Fabrice Sznajderman <fabszn@protonmail.com>
2019-05-17 15:18:01 +0200

    fix env production 4

--- src/app/index.js
+++ src/app/index.js
@@ -6,7 +6,7 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { routes } from './routeur-config.js';
 import Dropdown from 'vue-simple-search-dropdown';
-(require('dotenv').config({ silent: process.env.NODE_ENV === 'production' }))
+require('dotenv').config({ silent: process.env.NODE_ENV === 'production' })
 
 
 
