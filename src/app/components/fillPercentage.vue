9001717637ea8828c33649cdc27ff8fa22eac6ff
Fabrice Sznajderman <fabszn@protonmail.com>
2019-05-19 00:49:14 +0200

    add pass param in components

--- src/app/components/fillPercentage.vue
+++ src/app/components/fillPercentage.vue
@@ -1,5 +1,46 @@
 <template>
-  <div class="container-fluid">Fill percentage : {{id}}</div>
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-6">
+        <button type="button" class="btn btn-secondary btn-lg block-green" v-on:click="hit(10)">10%</button>
+      </div>
+      <div class="col-6">
+        <button type="button" class="btn btn-secondary btn-lg block-green" v-on:click="hit(20)">20%</button>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-6">
+        <button type="button" class="btn btn-secondary btn-lg block-green" v-on:click="hit(30)">30%</button>
+      </div>
+      <div class="col-6">
+        <button type="button" class="btn btn-secondary btn-lg block-green" v-on:click="hit(40)">40%</button>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-6">
+        <button type="button" class="btn btn-secondary btn-lg block-orange" v-on:click="hit(50)">50%</button>
+      </div>
+      <div class="col-6">
+        <button type="button" class="btn btn-secondary btn-lg block-orange" v-on:click="hit(60)">60%</button>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-6">
+        <button type="button" class="btn btn-secondary btn-lg block-orange" v-on:click="hit(70)">70%</button>
+      </div>
+      <div class="col-6">
+        <button type="button" class="btn btn-secondary btn-lg block-red" v-on:click="hit(80)">80%</button>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-6">
+        <button type="button" class="btn btn-secondary btn-lg block-red" v-on:click="hit(90)">90%</button>
+      </div>
+      <div class="col-6">
+        <button type="button" class="btn btn-secondary btn-lg block-red" v-on:click="hit(100)">Full</button>
+      </div>
+    </div>
+  </div>
 </template>
 <script>
 module.exports = {
@@ -10,10 +51,48 @@ module.exports = {
       id: this.slotId
     };
   },
-  methods: {}
+  methods: {
+    hit: function(perc) {
+      console.log(perc);
+    }
+  }
 };
 </script>
 
-<style>
-</style>
+<style  scoped>
+.block-green {
+  display: block;
+  width: 100%;
+  border: none;
+  background-color: #4caf50;
+  padding: 14px 28px;
+  font-size: 16px;
+  cursor: pointer;
+  text-align: center;
+  margin-bottom: 4px;
+}
 
+.block-orange {
+  display: block;
+  width: 100%;
+  border: none;
+  background-color: #ffa500;
+  padding: 14px 28px;
+  font-size: 16px;
+  cursor: pointer;
+  text-align: center;
+  margin-bottom: 4px;
+}
+
+.block-red {
+  display: block;
+  width: 100%;
+  border: none;
+  background-color: red;
+  padding: 14px 28px;
+  font-size: 16px;
+  cursor: pointer;
+  text-align: center;
+  margin-bottom: 4px;
+}
+</style>
