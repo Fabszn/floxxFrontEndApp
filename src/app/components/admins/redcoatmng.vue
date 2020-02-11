50950d102d19c80cfa61567142d6ef480214b91e
Fabrice Sznajderman <fabszn@protonmail.com>
2020-02-11 08:36:44 +0100

    new screens

---
+++ src/app/components/admins/redcoatmng.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    gestion des redcoats
+    - reaffectation d'une salle sur un redcoats
+    - lister les talks d'un redcoats
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+</style>
---
+++ src/app/components/admins/statistiques.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <ul>
+      <li>nombre de en fonction du temps</li>
+      <li>sur sélection d'un slots, affichage de la courbe de remplissage</li>
+      <li>...</li>
+    </ul>
+  </div>
+</template>
