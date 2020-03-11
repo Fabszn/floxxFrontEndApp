bd2971a33f6c026b39eb2309fd66ebbb037b5588
Fabrice Sznajderman <fabszn@protonmail.com>
2020-03-11 18:18:45 +0100

    first table with stats

--- src/app/components/admins/statistiques.vue
+++ src/app/components/admins/statistiques.vue
@@ -1,9 +1,48 @@
 <template>
   <div>
     <ul>
-      <li>nombre de en fonction du temps</li>
-      <li>sur sélection d'un slots, affichage de la courbe de remplissage</li>
+      <li>nombre de hit en fonction du temps</li>
+      <li>sur sélection d'un slots, affichage de la courbe de remplissage....</li>
       <li>...</li>
     </ul>
+    <b-table
+      head-variant="light"
+      dark="true"
+      details-td-class="cell"
+      responsive="true"
+      striped
+      hover
+      :items="items"
+    ></b-table>
   </div>
 </template>
+
+
+
+<script>
+import shared from "../../shared";
+export default {
+  data() {
+    return {
+      items: []
+    };
+  },
+  created: function() {
+    shared.securityAccess(this.$router, p => {
+      this.$http
+        .get(BACKEND_URL + "api/stats/slots", {
+          headers: shared.tokenHandle()
+        })
+        .then(p => {
+          this.items = p.data;
+        });
+    });
+  }
+};
+</script>
+
+<style scoped>
+.cell {
+  color: blanchedalmond;
+}
+</style>
